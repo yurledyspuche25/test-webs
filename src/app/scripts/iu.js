26a71f0b83a96a8ae28a626a310e296f90aa3a18
yurledyspuche25 <89873596+yurledyspuche25@users.noreply.github.com>
2022-12-15 15:55:52 -0500

    first commit

---
+++ src/app/scripts/iu.js
@@ -0,0 +1,4 @@
+export const demo =() =>{
+
+    console.log('hola IU');
+}
---
+++ src/app/scripts/main.js
@@ -0,0 +1,10 @@
+import { demo } from "./iu.js";
+import '../style/style.js'
+console.log('hola mundo');
+
+const sum = ()=>{
+    return 2+3;
+
+}
+console.log(sum());
+demo();
